920fdf8dd7b5d3af98bda9f9f8b7ddd403f2a4a7
sangramdesai123 <sangram.desai@somaiya.edu>
2018-12-09 13:33:38 +0530

    config file and image

---
+++ linked/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("CLIENT_ID", "XXXXXXXXXXX");
+define("CLIENT_SECRET", "XXXXXXXXXXx");
+define("REDIRECT_URI", "http://localhost/Social_Media_Api/linked/index.php");
+define("SCOPE", 'r_basicprofile r_emailaddress' );
+
+?>

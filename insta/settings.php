7caa0838dba0f99886f3ef88e2e89aa0c9bb8983
sangramdesai123 <sangram.desai@somaiya.edu>
2019-01-19 21:10:52 +0530

    login with Instagram

---
+++ insta/settings.php
@@ -0,0 +1,7 @@
+<?php
+	$settings = array(
+		"clientID" => "6e62af8e988f4d37832a9479fb107292",
+		"clientSecret" => "e8af8bb18f524f66a9fc48ebdfff28c7",
+		"redirectURI" => "Http://localhost/insta/callback.php"
+	);
+?>
